a0a6b1f3d901da0313f56314324b851f54e8267e
Ana Luiza <88158868+Nalu2@users.noreply.github.com>
2023-04-25 08:06:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function retornarValores(conteudo){
+    if(!("erro" in conteudo)){
+        document.getElementById('endereco').value = (conteudo.logradouro)
+        document.getElementById('inputbairro').value = (conteudo.bairro)
+        document.getElementById('inputcomp').value = (conteudo.complemento)
+        document.getElementById('inputcidade').value = (conteudo.localidade)
+    }
+    else {
+        alert("CEP não encontrado")
+    }
+}
 
+function buscaCep(valor){
+    var cep = valor.replace(/\D/g, '')
+    if(cep != ""){
+        var validarCEP = /^[0-9]{8}$/ 
+        if(validarCEP.test(cep)){
+            document.getElementById('endereco').value = "..."
+            var script = document.createElement('script')
+            script.src = `https://viacep.com.br/ws/${cep}/json/?callback=retornarValores`
+            document.body.appendChild(script)
+        }else{
+            alert("Formato invalido")
+        }
+    }
+}
